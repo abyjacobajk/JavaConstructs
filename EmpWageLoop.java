dd2ac46bb19be1c70236e6d35445e7053b13e953
aby jacob <abyjacobajk@gmail.com>
2020-09-28 22:02:30 +0530

    Compute wage for 20 days or 100 hours

---
+++ EmpWageLoop.java
@@ -0,0 +1,40 @@
+public class EmpWageLoop {
+        // Constants
+        public static final int WORK_HOURS=0;
+        public static final int IS_FULL_TIME = 2;
+	public static final int IS_PART_TIME = 1;
+        public static final int EMPLOYEE_RATE_PER_HOUR = 100;
+
+        public static void main(String[] args) {
+                // TODO Auto-generated method stub
+
+                // Variables
+                int totalHours=0;
+                int totalDays=0;
+		int hour=100;
+
+                double employeeCheck = Math.floor(Math.random()*10)%3;  // 0-2
+                System.out.println("generated - "+employeeCheck);
+
+                // TYPE CASTING - external
+                int employeeCheck1 = (int) (Math.floor(Math.random()*10)%3);
+                System.out.println("integer generated - "+employeeCheck1);
+
+		for(int i=0; i<=hour; i++){
+		if(employeeCheck == IS_FULL_TIME) {
+                      totalHours=10;
+              }
+              else if(employeeCheck == IS_PART_TIME){
+                      totalHours=5;
+              }
+              else {
+                      totalHours=0;
+              }
+
+		int WORK_HOURS=totalHours;
+	     }
+               System.out.println("wage - "+WORK_HOURS*EMPLOYEE_RATE_PER_HOUR*totalDays);
+ 	       }
+}
+
+
