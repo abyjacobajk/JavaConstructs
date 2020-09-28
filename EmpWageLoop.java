f24de5b7d808a2618c4683b5ca21248f69c620ad
aby jacob <abyjacobajk@gmail.com>
2020-09-28 22:42:09 +0530

    Compute wage for 20 days or 100 hours

--- EmpWageLoop.java
+++ EmpWageLoop.java
@@ -1,6 +1,6 @@
 public class EmpWageLoop {
         // Constants
-        public static final int WORK_HOURS=0;
+        public static final int WORK_HOURS=1;
         public static final int IS_FULL_TIME = 2;
 	public static final int IS_PART_TIME = 1;
         public static final int EMPLOYEE_RATE_PER_HOUR = 100;
@@ -10,30 +10,29 @@ public class EmpWageLoop {
 
                 // Variables
                 int totalHours=0;
-                int totalDays=0;
+                int totalDays=20;
 		int hour=100;
 
                 double employeeCheck = Math.floor(Math.random()*10)%3;  // 0-2
                 System.out.println("generated - "+employeeCheck);
 
-                // TYPE CASTING - external
-                int employeeCheck1 = (int) (Math.floor(Math.random()*10)%3);
-                System.out.println("integer generated - "+employeeCheck1);
+              
 
 		for(int i=0; i<=hour; i++){
-		if(employeeCheck == IS_FULL_TIME) {
-                      totalHours=10;
-              }
-              else if(employeeCheck == IS_PART_TIME){
-                      totalHours=5;
-              }
-              else {
-                      totalHours=0;
-              }
+ 			if(employeeCheck == IS_FULL_TIME) {
+        	              totalHours=10;
+                	}
+                	else if(employeeCheck == IS_PART_TIME){
+                      	      totalHours=5;
+              		}
+              		else {
+                      	      totalHours=0;
+             		 }
 
-		int WORK_HOURS=totalHours;
-	     }
-               System.out.println("wage - "+WORK_HOURS*EMPLOYEE_RATE_PER_HOUR*totalDays);
+			int WORK_HOURS=totalHours;
+	     	}
+		System.out.println("hour -"+WORK_HOURS*totaldays);
+                System.out.println("wage - "+WORK_HOURS*EMPLOYEE_RATE_PER_HOUR*totalDays);
  	       }
 }
 
